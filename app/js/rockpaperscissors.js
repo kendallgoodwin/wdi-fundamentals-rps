07c7d207ce7fe8a7c0ea736c8ea143f82842ea6d
Kendall Goodwin <kendall@kendallgoodwin.com>
2016-03-05 18:51:50 -0800

    expressions to evaluate function getWinner

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -43,6 +43,30 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    if (playerMove === computerMove) {
+        winner = 'tie';
+    }
+    else if (playerMove === 'rock' && computerMove === 'scissors') {
+        winner = 'player';
+    }
+    else if (playerMove === 'rock' && computerMove === 'paper') {
+        winner = 'computer';
+    }
+    else if (playerMove === 'scissors' && computerMove === 'rock') {
+        winner = 'computer';
+    }
+    else if (playerMove === 'scissors' && computerMove === 'paper') {
+        winner = 'player';
+    }
+    else if (playerMove === 'paper' && computerMove == 'rock') {
+        winner = 'player';
+    }
+    else if (playerMove === 'paper' && computerMove === 'scissors') {
+        winner = 'computer';
+    }
+    else {
+        winner = 'nope'
+    }
     return winner;
 }
 
